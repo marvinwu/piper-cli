2d68a4c0c47dbec9d1eba4ca87a55db5dbf2e68e
Marvin Wu <marvin.wu@urbanspring.hk>
2021-08-03 20:03:46 +0800

    working

--- test/cli/test.sh
+++ test/cli/test.sh
@@ -1,2 +1,4 @@
 cat test/input/html-to-text.json | bin/cli 'html-to-text' -p html -a text
+cat test/input/nlp1.json | bin/cli compromise.topics.json -p text -a nlp
+cat test/input/nlp1.json | bin/cli wink-nlp.entities.out -p text -a nlp
 # {"html":"<h1>Hello World</h1>","text":"HELLO WORLD"}
